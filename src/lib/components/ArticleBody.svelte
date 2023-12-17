7e682dc765321dbf3cf8b76de7baa486d57f5973
Ryan Kent <ryan@interphased.com>
2023-12-17 15:28:25 -0500

    rm line-height on p first-letter

--- src/lib/components/ArticleBody.svelte
+++ src/lib/components/ArticleBody.svelte
@@ -1,25 +1,24 @@
 <div class="article-body">
-  <slot />
+	<slot />
 </div>
 
 <style>
-  .article-body {
-    padding: 2rem 0 4rem 0;
-  }
-  :global(.article-body > p:first-child::first-letter) {
-    font-size: 5rem;
-    line-height: 0;
-    float: left;
-    margin-top: 0rem;
-    margin-right: 0.1rem;
-    font-family: var(--font-serif);
-  }
-  .article-body :global(h2) {
-    position: relative;
-    margin: 6rem 0 2rem 0;
-  }
+	.article-body {
+		padding: 2rem 0 4rem 0;
+	}
+	:global(.article-body > p:first-child::first-letter) {
+		font-size: 5rem;
+		float: left;
+		margin-top: 0;
+		margin-right: 0.1rem;
+		font-family: var(--font-serif);
+	}
+	.article-body :global(h2) {
+		position: relative;
+		margin: 6rem 0 2rem 0;
+	}
 
-  /* .article-body :global(h2::before) {
+	/* .article-body :global(h2::before) {
     content: '';
     display: block;
     width: 4rem;
@@ -28,63 +27,63 @@
     margin-bottom: 0.5rem;
   } */
 
-  .article-body :global(h3) {
-    margin-top: 3rem;
-    margin-bottom: 2rem;
-  }
+	.article-body :global(h3) {
+		margin-top: 3rem;
+		margin-bottom: 2rem;
+	}
 
-  .article-body :global(h4) {
-    margin-top: 3rem;
-    margin-bottom: 1rem;
-  }
+	.article-body :global(h4) {
+		margin-top: 3rem;
+		margin-bottom: 1rem;
+	}
 
-  .article-body :global(p) {
-    margin-left: 2rem;
-    margin-right: 2rem;
-  }
+	.article-body :global(p) {
+		margin-left: 2rem;
+		margin-right: 2rem;
+	}
 
-  .article-body :global(pre[class*="language-"]) {
-    margin: 2rem 0 2rem 0;
-    padding: 2rem;
-  }
+	.article-body :global(pre[class*='language-']) {
+		margin: 2rem 0 2rem 0;
+		padding: 2rem;
+	}
 
-  .article-body :global(pre[class*="language-"] > code[class*="language-"]) {
-    white-space: pre-wrap;
-    word-break: break-word;
-  }
+	.article-body :global(pre[class*='language-'] > code[class*='language-']) {
+		white-space: pre-wrap;
+		word-break: break-word;
+	}
 
-  .article-body :global(blockquote) {
-    margin: 2rem 0;
-    background: var(--blockquote-bg-color);
-    padding: 2rem 3rem;
-    border-radius: 0.5rem;
-    color: var(--heading-color);
-  }
+	.article-body :global(blockquote) {
+		margin: 2rem 0;
+		background: var(--blockquote-bg-color);
+		padding: 2rem 3rem;
+		border-radius: 0.5rem;
+		color: var(--heading-color);
+	}
 
-  .article-body :global(blockquote p) {
-    font-style: italic;
-  }
+	.article-body :global(blockquote p) {
+		font-style: italic;
+	}
 
-  .article-body :global(blockquote h1),
-  .article-body :global(blockquote h2),
-  .article-body :global(blockquote h3),
-  .article-body :global(blockquote h4),
-  .article-body :global(blockquote p) {
-    margin: 0;
-    color: var(--heading-color);
-  }
+	.article-body :global(blockquote h1),
+	.article-body :global(blockquote h2),
+	.article-body :global(blockquote h3),
+	.article-body :global(blockquote h4),
+	.article-body :global(blockquote p) {
+		margin: 0;
+		color: var(--heading-color);
+	}
 
-  @media screen and (max-width: 40rem) {
-    .article-body :global(p),
-    .article-body :global(h2) {
-      margin-left: 0;
-      margin-right: 0;
-    }
-    .article-body :global(blockquote) {
-      padding: 1rem;
-    }
-    .article-body :global(pre[class*="language-"]) {
-      padding: 1rem;
-    }
-  }
+	@media screen and (max-width: 40rem) {
+		.article-body :global(p),
+		.article-body :global(h2) {
+			margin-left: 0;
+			margin-right: 0;
+		}
+		.article-body :global(blockquote) {
+			padding: 1rem;
+		}
+		.article-body :global(pre[class*='language-']) {
+			padding: 1rem;
+		}
+	}
 </style>
